42d3d6d3c088527582eec2e32e7f78104f9ef194
Victoria Crespin <crespinvictoria2@gmail.com>
2024-12-19 05:38:48 -0300

    ej1

---
+++ ej1.js
@@ -0,0 +1,3 @@
+// ej1: Mostrar por consola, tu nombre, apellido, comisión y hobbies
+
+console.log('Mi nombre es Victoria Crespin, mi comisión es la 24234 y mis hobbies son ir al gimnasio y jugar videojuegos')
